e80f0acb367924b697f297f079e50bbc97684281
Alexander Rehnman <globox85@gmail.com>
2016-11-16 13:09:01 +0100

    Created a basic participant object

---
+++ SecretSanta/SecretSanta/SecretSantaParticipant.cs
@@ -0,0 +1,66 @@
+﻿namespace SecretSanta
+{
+    internal class SecretSantaParticipant
+    {
+        private string name;
+        private SecretSantaParticipant receiver, santa;
+        private bool hasSanta = false;
+        private bool hasReceiver = false;
+        public bool HasSanta
+        {
+            get
+            {
+                return hasSanta;
+            }
+
+            set
+            {
+                hasSanta = value;
+            }
+        }
+        public bool HasReceiver
+        {
+            get
+            {
+                return hasReceiver;
+            }
+            set
+            {
+                hasReceiver = value;
+            }
+        }
+        public SecretSantaParticipant(string n)
+        {
+            name = n;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setName(string n)
+        {
+            name = n;
+        }
+
+        public SecretSantaParticipant getSanta()
+        {
+            return santa;
+            
+        }
+
+        public void setSanta(SecretSantaParticipant s)
+        {
+            santa = s;
+            hasSanta = true;
+        }
+
+        public void setReceiver(SecretSantaParticipant g)
+        {
+            receiver = g;
+            hasReceiver = true;
+        }
+        
+    }
+}
